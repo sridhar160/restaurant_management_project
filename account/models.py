bee99e4c4130b53fd5593be813f79aafad32168b
sridhar <naralasridhar214@gmail.com>
2026-03-11 11:15:01 +0530

    Model Created

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
-from django.db import models
 
-# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
